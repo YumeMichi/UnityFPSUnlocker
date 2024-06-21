99dc374f5ca48dd5308e58957ec716aeba9bd55c
Yuan Si <do4suki@gmail.com>
2024-06-21 09:01:00 +0800

    Support resolution settings for PJSK
    The current SetResolution function doesn't work on PJSK due to a different function prototype. Let's use SetResolution_Injected instead.
    
    // Dll : UnityEngine.CoreModule.dll
    // Namespace: UnityEngine
    public struct RefreshRate : IEquatable`1, IComparable`1
    {
    	// Fields
    	public UInt32 numerator; // 0x10
    	public UInt32 denominator; // 0x14
    
    	// ...
    }
    
    // RVA: 0x6ead488 VA: 0x744e243488
    public static Void SetResolution(Int32 width, Int32 height, FullScreenMode fullscreenMode, RefreshRate preferredRefreshRate) { }
    // RVA: 0x6ead544 VA: 0x744e243544
    public static Void SetResolution(Int32 width, Int32 height, Boolean fullscreen, Int32 preferredRefreshRate) { }
    // RVA: 0x6ead5b4 VA: 0x744e2435b4
    public static Void SetResolution(Int32 width, Int32 height, Boolean fullscreen) { }
    // RVA: 0x6ead378 VA: 0x744e243378
    private static Void get_currentResolution_Injected(out Resolution ret) { }
    // RVA: 0x6ead4e8 VA: 0x744e2434e8
    private static Void SetResolution_Injected(Int32 width, Int32 height, FullScreenMode fullscreenMode, ref RefreshRate preferredRefreshRate) { }
    
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- UnityFPSUnlocker/unity/unity_engine.cc
+++ UnityFPSUnlocker/unity/unity_engine.cc
@@ -7,25 +7,24 @@
 
 void Unity::Init(void* handle) {
     if ((il2cpp_resolve_icall = (il2cpp_resolve_icall_f)xdl_sym(handle, "il2cpp_resolve_icall", nullptr)) == nullptr) {
-        ERROR("il2cpp_resolve_icall not fount");
+        ERROR("il2cpp_resolve_icall not found");
     }
 
     if ((set_targetFrameRate = (set_targetFrameRate_f)il2cpp_resolve_icall("UnityEngine.Application::set_targetFrameRate")) == nullptr) {
-        ERROR("set_targetFrameRate not fount");
+        ERROR("set_targetFrameRate not found");
     }
 
     if ((get_currentResolution = (get_currentResolution_t)il2cpp_resolve_icall("UnityEngine.Screen::get_currentResolution_Injected")) == nullptr) {
-        ERROR("get_currentResolution not fount");
+        ERROR("get_currentResolution not found");
     }
 
-    if ((SetResolution_Internal = (SetResolution_t)il2cpp_resolve_icall("UnityEngine.Screen::SetResolution")) == nullptr) {
-        if ((SetResolution_Internal = (SetResolution_t)il2cpp_resolve_icall("UnityEngine.Screen::SetResolution_Injected")) == nullptr) {
-            ERROR("SetResolution not fount");
-        }
+    if ((SetResolution_Internal = (SetResolution_t)il2cpp_resolve_icall("UnityEngine.Screen::SetResolution")) == nullptr &&
+        (SetResolution_Injected_Internal = (SetResolution_Injected_t)il2cpp_resolve_icall("UnityEngine.Screen::SetResolution_Injected")) == nullptr) {
+        ERROR("SetResolution not found");
     }
 
     if ((GetSystemExtImpl_Internal = (GetSystemExtImpl_t)il2cpp_resolve_icall("UnityEngine.Display::GetSystemExtImpl")) == nullptr) {
-        ERROR("GetSystemExtImpl not fount");
+        ERROR("GetSystemExtImpl not found");
     }
 }
 
@@ -47,12 +46,21 @@ Resolution Unity::GetResolution() {
 
 void Unity::SetResolution(float scale) {
     Resolution resolution = GetSystemExtImpl();
-    if (SetResolution_Internal && scale > 0 && resolution.m_Width > 0) {
+    if ((SetResolution_Internal || SetResolution_Injected_Internal) && scale > 0 && resolution.m_Width > 0) {
         auto target_width = static_cast<int32_t>(resolution.m_Width * scale);
         auto target_height = static_cast<int32_t>(resolution.m_Height * scale);
-        LOG("Set resolution: %d x %d", target_width, target_height);
-        SetResolution_Internal(target_width, target_height, 1, 0);
-        Utility::NopFunc(reinterpret_cast<unsigned char*>(SetResolution_Internal));
+        LOG("Set resolution: %dx%d", target_width, target_height);
+
+        if (SetResolution_Injected_Internal) {
+            RefreshRate refreshRate;
+            SetResolution_Injected_Internal(target_width, target_height, 1, &refreshRate);
+            Utility::NopFunc(reinterpret_cast<unsigned char*>(SetResolution_Injected_Internal));
+        }
+
+        if (SetResolution_Internal) {
+            SetResolution_Internal(target_width, target_height, 1, 0);
+            Utility::NopFunc(reinterpret_cast<unsigned char*>(SetResolution_Internal));
+        }
     }
 }
 
@@ -61,6 +69,7 @@ void Unity::SetFrameRate(int framerate, bool mod_opcode) {
     if (set_targetFrameRate) {
         set_targetFrameRate(framerate);
         LOG("Current resolution: %dx%d @%d", resolution.m_Width, resolution.m_Height, resolution.m_RefreshRate);
+        LOG("Set framerate: %d", framerate);
         if (framerate > resolution.m_RefreshRate) {
             ERROR("The screen refresh rate is lower than target framerate! %d < %d", resolution.m_RefreshRate, framerate);
         }
--- UnityFPSUnlocker/unity/unity_engine.hh
+++ UnityFPSUnlocker/unity/unity_engine.hh
@@ -11,6 +11,11 @@ struct Resolution {
     int32_t m_RefreshRate = 0;
 };
 
+struct RefreshRate {
+    int32_t numerator = 0;
+    int32_t denominator = 0;
+};
+
 class Unity : public Singleton<Unity> {
 public:
     void Init(void*);
@@ -24,12 +29,14 @@ private:
     using il2cpp_resolve_icall_f = void* (*)(const char*);
     using set_targetFrameRate_f = void (*)(int);
     using SetResolution_t = void (*)(int, int, int, int);
+    using SetResolution_Injected_t = void (*)(int32_t, int32_t, int32_t, RefreshRate*);
     using get_currentResolution_t = void (*)(Resolution*);
     using GetSystemExtImpl_t = void (*)(void*, int*, int*);
 
     il2cpp_resolve_icall_f il2cpp_resolve_icall = nullptr;
     set_targetFrameRate_f set_targetFrameRate = nullptr;
     SetResolution_t SetResolution_Internal = nullptr;
+    SetResolution_Injected_t SetResolution_Injected_Internal = nullptr;
     get_currentResolution_t get_currentResolution = nullptr;
     GetSystemExtImpl_t GetSystemExtImpl_Internal = nullptr;
 };
